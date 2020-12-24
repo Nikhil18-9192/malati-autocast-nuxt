bd22560ab27b521f2fb2a04889f0a0cb1309ec21
Nikhil18-9192 <nikhilkorane08@example.com>
2020-12-24 10:56:56 +0530

    init

---
+++ layouts/default.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <Nuxt />
+  </div>
+</template>
+
+<style lang="scss" scopped>
+* {
+  box-sizing: border-box;
+}
+
+body {
+  margin: 0;
+  padding: 0;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <div id="Home-page">test</div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style></style>
